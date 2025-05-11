4daeca63e0559a0ced05a72868fe50a3c5fa3972
VishalPcode <vishalgupta732160@gmail.com>
2025-05-11 14:14:41 +0530

    Webpack updated with configuration

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+const AppComponent = () =>  {
+    return(
+    <div>
+        <div className="container">
+        App Component     
+        </div>    
+    </div>
+    )
+}
+
+export default AppComponent;
---
+++ webpack.development.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/components/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].bundle.js',
+      },
+      resolve: {
+        extensions: ['.js', '.jsx'],
+        alias:{
+            '@': path.resolve(__dirname,src),
+        }
+      },
+      module: {
+        rules: [
+          {
+            test: /\.(?:js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  ['@babel/preset-react']
+                ]
+              }
+            }
+          }
+        ]
+      }
+       
+  };
